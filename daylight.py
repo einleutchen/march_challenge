f3f0d636bebf38c2fd846cb6beca41037538a1d6
uw011235 <uyenwolff@gmail.com>
2026-03-09 13:11:52 +0100

    tracking how much sleep debt everyone racks up before launch
    Added docstring to calculate_sleep_debt function for clarity.

--- daylight.py
+++ daylight.py
@@ -1,5 +1,14 @@
 def calculate_sleep_debt(planned, actual):
   # Write code below 💖
+  """
+  Tracking sleep debt
+  Args:
+  planned (list): intended hours to sleep
+  actual (list): actually sleep hours
+  Returns:
+  total sleep debt for the week (including +1 Daylight Savings hour)
+  longest streak of consecutive days with sleep debt
+  """
   debt = [planned[i] - actual[i] for i in range(len(planned))]
   sleep_debt = [0 if num < 0 else num for num in debt]
   time_diff = list()
@@ -13,4 +22,5 @@ def calculate_sleep_debt(planned, actual):
       continue
   return sum(sleep_debt)+1, len(time_diff)+1
 
+# example
 print(calculate_sleep_debt([7.5, 8, 7.5, 8, 8.5, 8, 7.5], [5, 12, 6, 6, 9, 8, 6.5]))
